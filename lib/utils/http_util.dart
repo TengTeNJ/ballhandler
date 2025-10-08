bdff87684bb8039da59e300b3eda42ecc9f73a3f
杨庆杰 <1534760657@qq.com>
2025-10-08 15:45:53 +0800

    iOS解决收不到推送消息的问题（Googleservice-info.plist文件内容不正确）

--- lib/utils/http_util.dart
+++ lib/utils/http_util.dart
@@ -54,14 +54,19 @@ class HttpUtil {
         TTToast.hideLoading();
       }
       final _code = response.data['code'];
-      if ( response.data!= null && _code != null && _code == '0' ) {
+      if ( response.data!= null && _code != null && _code == '0' && response.data is Map ) {
         return ApiResponse(
             success: true, data: response.data, errorMessage: 'success');
       } else {
         if (showLoading) {
           TTToast.showErrorInfo(response.data['msg']);
         }
-        _handleFailure(int.parse(_code));
+        if(_code != null){
+          _handleFailure(int.parse(_code));
+        }else{
+          TTToast.showErrorInfo('code is null');
+        }
+
         return ApiResponse(
             success: false, data: response.data, errorMessage: 'false');
       }
