1b87f61745bb52c3ef81734e3f642e8f82de315e
杨庆杰 <1534760657@qq.com>
2025-10-16 18:34:05 +0800

    优化Stats页面(下拉框和数据请求逻辑)

--- lib/controllers/stats/stats_home_controller.dart
+++ lib/controllers/stats/stats_home_controller.dart
@@ -1,16 +1,20 @@
+import 'package:code/services/http/participants.dart';
 import 'package:code/views/stats/line_thumbnail_view.dart';
 import 'package:code/views/stats/milestone_thumbnail_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 import '../../constants/constants.dart';
 import '../../models/mystats/my_stats_model.dart';
 import '../../services/http/rank.dart';
 import '../../utils/color.dart';
 import '../../utils/dialog.dart';
+import '../../utils/global.dart';
 import '../../utils/string_util.dart';
 import '../../utils/system_device.dart';
 import '../../views/airbattle/my_stats_grid_list_view.dart';
 import '../../widgets/navigation/CustomAppBar.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class StatsHomeController extends StatefulWidget {
   const StatsHomeController({super.key});
@@ -23,6 +27,9 @@ class _StatsHomeControllerState extends State<StatsHomeController> {
   List<MyStatsModel> datas = [];
   List<MyStatsModel> barViewDatas = [];
   AnalyzeDataModel _analyzeDataModelmodel = AnalyzeDataModel();
+  late String selectedValue;
+  late int currentIndex;
+  late GameScene initGameScene;// 进页面时记录的
   List<String> _titles = [
     'Last 7 days',
     'Last 30 days',
@@ -98,16 +105,24 @@ class _StatsHomeControllerState extends State<StatsHomeController> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    GameUtil gameUtil = GetIt.instance<GameUtil>();
+    initGameScene = gameUtil.gameScene;
+
+    final result = gameUtil.sceneList.firstWhere((item) => item.dictKey == (gameUtil.gameScene.index + 1).toString());
+    selectedValue = result.dictValue;
+    currentIndex = int.parse(result.dictKey);
     initData();
   }
 
   @override
   Widget build(BuildContext context) {
+    GameUtil gameUtil = GetIt.instance<GameUtil>();
+    print('gameUtil.sceneList${gameUtil.sceneList}');
     return Scaffold(
       backgroundColor: Constants.darkThemeColor,
       appBar: CustomAppBar(),
       body: Container(
-        margin: EdgeInsets.only(left: 16,right: 16),
+        margin: EdgeInsets.only(left: 16, right: 16),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -122,20 +137,59 @@ class _StatsHomeControllerState extends State<StatsHomeController> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    children: [
-                      Image(
-                        image: AssetImage('images/ranking/top_rank.png'),
-                        width: 14,
-                        height: 17,
-                      ),
-                      SizedBox(
-                        width: 6,
-                      ),
-                      Constants.regularWhiteTextWidget(
-                          ' Ranking #${_analyzeDataModelmodel.rankNumber == '0' ? '-' : _analyzeDataModelmodel.rankNumber}',
-                          16),
-                    ],
+                  DropdownButtonHideUnderline(
+                    child: DropdownButton2(
+                        isExpanded: true,
+                        value: selectedValue,
+                        buttonStyleData: ButtonStyleData(
+                          height: 24,
+                          width: 200,
+                          padding: const EdgeInsets.only(left: 12, right: 0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14),
+                            border: Border.all(
+                              color: hexStringToColor('#707070'),
+                            ),
+                            color: hexStringToColor('#292936'),
+                          ),
+                          elevation: 2,
+                        ),
+                        dropdownStyleData: DropdownStyleData(
+                          maxHeight: 200,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14),
+                            color: hexStringToColor('#292936'),
+                          ),
+                          offset: const Offset(0, 0),
+                          scrollbarTheme: ScrollbarThemeData(
+                            radius: const Radius.circular(40),
+                            thickness: MaterialStateProperty.all(6),
+                            thumbVisibility: MaterialStateProperty.all(true),
+                          ),
+                        ),
+                        menuItemStyleData: const MenuItemStyleData(
+                          height: 40,
+                          padding: EdgeInsets.only(left: 14, right: 14),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedValue = value!;
+                            final result = gameUtil.sceneList.firstWhere((item) => item.dictValue == value);
+                            print('value=${value}');
+                            print('${result.dictKey}-${result.dictValue}');
+                            gameUtil.gameScene = GameScene.values[int.parse(result.dictKey) - 1];
+                            initData();
+                            changeTimeArea(
+                                _end.isNotEmpty ? _end : null, _start.isNotEmpty ? _start : null, (_timeIndex + 1).toString());
+                          });
+                        },
+                        items: gameUtil.sceneList.map((SceneModel model) {
+                          return DropdownMenuItem(
+                              value: model.dictValue,
+                              child: Constants.regularWhiteTextWidget(
+                                  model.dictValue, 12));
+                        }).toList()),
                   ),
                   GestureDetector(
                     behavior: HitTestBehavior.opaque,
@@ -143,37 +197,37 @@ class _StatsHomeControllerState extends State<StatsHomeController> {
                       // 时间选择弹窗
                       if (await SystemUtil.isIPad()) {
                         TTDialog.iPadTimeSelect(context,
-                                (startTime, endTime, index) {
-                              _timeIndex = index;
-                              _start = startTime;
-                              _end = endTime;
-                              if (index == 3) {
-                                // _titles[_titles.length-1] = "${_start.replaceAll('-', '/')}-${_end.replaceAll('-', '/')}";
-                                _titles[_titles.length - 1] =
+                            (startTime, endTime, index) {
+                          _timeIndex = index;
+                          _start = startTime;
+                          _end = endTime;
+                          if (index == 3) {
+                            // _titles[_titles.length-1] = "${_start.replaceAll('-', '/')}-${_end.replaceAll('-', '/')}";
+                            _titles[_titles.length - 1] =
                                 "${StringUtil.serviceStringMyStatuDateString(_start)}-${StringUtil.serviceStringMyStatuDateString(_end)}";
-                                setState(() {});
-                              }
-                              changeTimeArea(
-                                  endTime, startTime, (index + 1).toString());
-                            },
+                            setState(() {});
+                          }
+                          changeTimeArea(
+                              endTime, startTime, (index + 1).toString());
+                        },
                             index: _timeIndex,
                             start: _start.length > 0 ? _start : null,
                             end: _end.length > 0 ? _end : null);
                       } else {
                         TTDialog.timeSelect(context,
-                                (startTime, endTime, index) {
-                              _timeIndex = index;
-                              _start = startTime;
-                              _end = endTime;
-                              if (index == 3) {
-                                //_titles[_titles.length-1] = "${_start.replaceAll('-', '/')}-${_end.replaceAll('-', '/')}";
-                                _titles[_titles.length - 1] =
+                            (startTime, endTime, index) {
+                          _timeIndex = index;
+                          _start = startTime;
+                          _end = endTime;
+                          if (index == 3) {
+                            //_titles[_titles.length-1] = "${_start.replaceAll('-', '/')}-${_end.replaceAll('-', '/')}";
+                            _titles[_titles.length - 1] =
                                 "${StringUtil.serviceStringMyStatuDateString(_start)}-${StringUtil.serviceStringMyStatuDateString(_end)}";
-                                setState(() {});
-                              }
-                              changeTimeArea(
-                                  endTime, startTime, (index + 1).toString());
-                            },
+                            setState(() {});
+                          }
+                          changeTimeArea(
+                              endTime, startTime, (index + 1).toString());
+                        },
                             index: _timeIndex,
                             start: _start.length > 0 ? _start : null,
                             end: _end.length > 0 ? _end : null);
@@ -213,7 +267,9 @@ class _StatsHomeControllerState extends State<StatsHomeController> {
                 model: _analyzeDataModelmodel,
                 selectType: _timeIndex,
               ),
-              const SizedBox(height: 8,),
+              const SizedBox(
+                height: 8,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -226,5 +282,14 @@ class _StatsHomeControllerState extends State<StatsHomeController> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    GameUtil gameUtil = GetIt.instance<GameUtil>();
+    gameUtil.gameScene =  initGameScene;
+    print('gameUtil.gameScene=${gameUtil.gameScene}');
+    super.dispose();
   }
 }
